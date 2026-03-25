f5140d62bc96ce94673c2d910fa71da3e0d6b361
machindra7 <150156282+machindra7@users.noreply.github.com>
2026-03-25 13:36:14 +0000

    sjhgd

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' 
+
 const awsConfig = {
   region: import.meta.env.VITE_AWS_REGION,
   userPoolId: import.meta.env.VITE_USER_POOL_ID,
   userPoolWebClientId: import.meta.env.VITE_CLIENT_ID
 };
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' 
+Amplify.configure(awsConfig);
+
+console.log("ENV:", import.meta.env);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
