9652656dae3b89c478f49222338b6984586d554a
machindra7 <150156282+machindra7@users.noreply.github.com>
2026-03-25 15:39:04 +0000

    sdhgl

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,9 @@ import App from './App.jsx'
 import './index.css' 
 
 import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-config';
 
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      region: "us-east-1",
-      userPoolId: "us-east-1_cs9G6OSca",
-      userPoolClientId: "3iqlm8kfho6j3afl04fa2i8fu4"
-    }
-  }
-});
-
-
-console.log("ENV:", import.meta.env);
+Amplify.configure(awsConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
