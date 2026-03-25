ae3c43549d86f5f7419265d915c09aa5589acc67
machindra7 <150156282+machindra7@users.noreply.github.com>
2026-03-25 15:44:45 +0000

    uaevf

---
+++ src/aws-config.js
@@ -0,0 +1,11 @@
+const awsConfig = {
+  Auth: {
+    Cognito: {
+      region: "us-east-1",
+      userPoolId: "us-east-1_cs9G6OSca",
+      userPoolClientId: "3iqlm8kfho6j3afl04fa2i8fu4"
+    }
+  }
+};
+
+export default awsConfig;
